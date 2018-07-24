4be28fa10d9d1ccbf4c7b98d2efb758420a59762
x <x@xs-MacBook-Pro-5.local>
2018-07-23 18:13:33 -0700

    Information display cleaned up

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var StatSchema = new mongoose.Schema({
     dp: { type: Number },
     axe: { type: String },
     name: { type: String },
-    image: { data: Buffer, contentType: String}
+    image: { data: Buffer, contentType: String }
 });
 
 mongoose.model('GearScore', StatSchema)
@@ -43,13 +43,10 @@ bot.on('message', message => {
 
     if (message.channel.type === 'dm') return message.channel.send('Hello');
 
-
     let prefix = botconfig.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-
-
     if (cmd.charAt(0) === prefix) {
 
         console.log(args)
@@ -60,131 +57,137 @@ bot.on('message', message => {
 
         //Set commands
         if ((cmd === prefix + 'stats') && (args[0] === 'set')) {
-
-
-
-
             if (args[1] === 'family') {
+
+                console.log(message.channel.id)
 
                 Stat.findOneAndUpdate({ id: message.author.id }, { family: args[2] }, { upsert: true }, function (err, data) {
                     if (err) {
-                        return message.channel.send('Somethings wrong, tell Jacky')
+                        message.channel.send('Somethings wrong, tell Jacky')
                     } else {
                         if (message.guild.members.get(bot.user.id).hasPermission("MANAGE_NICKNAMES") && message.guild.members.get(bot.user.id).hasPermission("CHANGE_NICKNAME")) {
                             message.guild.members.get(message.author.id).setNickname(args[2]);
                         } else {
                             message.channel.sendMessage("I dont have the permissons to change your nickname in this server. Should I get permission to do so, simply set your family name again to have your nickname changed to it. Or do it yourself. I don't care.");
                         }
-                        return message.channel.send('Family name set to ' + args[2])
+                        message.channel.send('Family name set to ' + args[2])
                     }
                 })
-
-
-
-
-
             }
-
             if (args[1] === 'AP') {
-
-
                 Stat.findOneAndUpdate({ id: message.author.id }, { ap: args[2] }, { upsert: true }, function (err, data) {
                     if (err) {
-                        return message.channel.send('Somethings wrong, tell Jacky')
+                        message.channel.send('Somethings wrong, tell Jacky')
                     } else {
-                        return message.channel.send('AP set to ' + args[2])
+                        message.channel.send('AP set to ' + args[2])
                     }
                 })
             }
-
-
-
-
             if (args[1] === 'name') {
 
                 Stat.findOneAndUpdate({ id: message.author.id }, { name: args[2] }, { upsert: true }, function (err, data) {
                     if (err) {
-                        return message.channel.send('Somethings wrong, tell Jacky')
+                        message.channel.send('Somethings wrong, tell Jacky')
                     } else {
-                        return message.channel.send('Name set to ' + args[2])
+                        message.channel.send('Name set to ' + args[2])
                     }
                 })
             }
-
-
             if (args[1] === 'class') {
 
                 Stat.findOneAndUpdate({ id: message.author.id }, { class: args[2] }, { upsert: true }, function (err, data) {
                     if (err) {
-                        return message.channel.send('Somethings wrong, tell Jacky')
+                        message.channel.send('Somethings wrong, tell Jacky')
                     } else {
-                        return message.channel.send('Class set to ' + args[2])
+                        message.channel.send('Class set to ' + args[2])
                     }
                 })
 
             }
-
             if (args[1] === 'level') {
 
                 Stat.findOneAndUpdate({ id: message.author.id }, { level: args[2] }, { upsert: true }, function (err, data) {
                     if (err) {
-                        return message.channel.send('Somethings wrong, tell Jacky')
+                        message.channel.send('Somethings wrong, tell Jacky')
                     } else {
-                        return message.channel.send('Level set to ' + args[2])
+                        message.channel.send('Level set to ' + args[2])
                     }
                 })
-
             }
-
-
             if (args[1] === 'Awakening') {
 
                 Stat.findOneAndUpdate({ id: message.author.id }, { awakening: args[2] }, { upsert: true }, function (err, data) {
                     if (err) {
-                        return message.channel.send('Somethings wrong, tell Jacky')
+                        message.channel.send('Somethings wrong, tell Jacky')
                     } else {
-                        return message.channel.send('Awakening AP set to ' + args[2])
+                        message.channel.send('Awakening AP set to ' + args[2])
                     }
                 })
-
             }
-
             if (args[1] === 'DP') {
 
                 Stat.findOneAndUpdate({ id: message.author.id }, { dp: args[2] }, { upsert: true }, function (err, data) {
                     if (err) {
-                        return message.channel.send('Somethings wrong, tell Jacky')
+                        message.channel.send('Somethings wrong, tell Jacky')
                     } else {
-                        return message.channel.send('DP set to ' + args[2])
+                        message.channel.send('DP set to ' + args[2])
                     }
                 })
-
             }
-
             if (args[1] === 'Trina_Axe') {
 
                 Stat.findOneAndUpdate({ id: message.author.id }, { axe: args[2] }, { upsert: true }, function (err, data) {
                     if (err) {
-                        return message.channel.send('Somethings wrong, tell Jacky')
+                        message.channel.send('Somethings wrong, tell Jacky')
                     } else {
-                        return message.channel.send('Trina Axe set to ' + args[2])
+                        message.channel.send('Trina Axe set to ' + args[2])
                     }
                 })
-
             }
-
-            if(args[1] === 'picture'){
-                if(message.attachments.size > 0){
+            if (args[1] === 'picture') {
+                if (message.attachments.size > 0) {
                     console.log('picture found')
                     message.channel.send('Found picture')
                     console.log(message.attachments['470043013717032967'])
                 } else {
                     message.channel.send('Where the fucks your picture dawg.')
                 }
-                
             }
-
         }
+        if (cmd === prefix + 'ranking') {
+            var humans = []
+            var gearedPeople = []
+
+            message.guild.members.forEach(function (guildMember, guildMemberId) {
+                if (guildMember.user.bot == false) {
+                    humans.push(guildMemberId)
+                }
+            })
+
+            // for(var i = 0; i < humans.length; i++){
+            //     console.log(humans[i])
+
+            //     var geared = []
+
+            //     Stat.find({id : humans[i]}, function(err, data){
+
+            //         if(err){
+            //             console.log('Error while searching for ' + humans[i])
+            //         } else {
+
+            //             geared.push(data[0]['id'])
+            //             gearedPeople = geared
+            //             console.log(data[0]['id'])
+
+            //         }
+
+            //     }
+
+            // )}
+
+            humans.forEach()
+        }
+
 
         if (cmd === prefix + 'gear') {
 
@@ -192,7 +195,7 @@ bot.on('message', message => {
 
             if (args[0] === 'of') {
 
-                console.log(message.mentions.members.first())
+                console.log(message.mentions.members.first()['user'])
 
                 var userId = message.mentions.members.first()['user']['id']
 
@@ -200,7 +203,42 @@ bot.on('message', message => {
                     if (err) {
                         console.log('Something went wrong, tell Jacky')
                     } else {
-                        return message.channel.send('Family name : ' + JSON.stringify(data[0]['family']) + '   AP : ' + JSON.stringify(data[0]['ap']) + '   DP : ' + JSON.stringify(data[0]['dp']) + '   Awakening AP : ' + JSON.stringify(data[0]['awakening']) + '   Class : ' + JSON.stringify(data[0]['class']) + '   Axe : ' + JSON.stringify(data[0]['axe']))
+                        if (data.length) {
+                            message.channel.send({
+                                embed: {
+                                    color: 3447003,
+                                    author: {
+                                        name: bot.user.username,
+                                        icon_url: bot.user.avatarURL
+                                    },
+                                    title: "Gear of " + data[0]['family'],
+                                    fields: [{
+                                        name: 'Class',
+                                        value: data[0]['class']
+                                    },
+
+                                    {
+                                        name: 'AP',
+                                        value: JSON.stringify(data[0]['ap'])
+                                    },
+                                    {
+                                        name: 'DP',
+                                        value: JSON.stringify(data[0]['dp'])
+                                    },
+                                    {
+                                        name: 'Awakening AP',
+                                        value: JSON.stringify(data[0]['awakening'])
+                                    },
+                                    {
+                                        name: 'Trina Axe',
+                                        value: JSON.stringify(data[0]['axe'])
+                                    }
+                                    ]
+                                }
+                            })
+                        } else {
+                            message.channel.send('This user has not entered any information yet.')
+                        }
                     }
                 })
 
@@ -209,10 +247,43 @@ bot.on('message', message => {
             } else {
                 Stat.find({ id: message.author.id }, function (err, data) {
                     if (err) {
-                        return message.channel.send('Cant find it or something went wrong. Tell Jacky')
+                        message.channel.send('Cant find it or something went wrong. Tell Jacky')
                     } else {
-                        console.log(JSON.stringify(data[0]))
-                        return message.channel.send('Family name : ' + JSON.stringify(data[0]['family']) + '   AP : ' + JSON.stringify(data[0]['ap']) + '   DP : ' + JSON.stringify(data[0]['dp']) + '   Awakening AP : ' + JSON.stringify(data[0]['awakening']) + '   Class : ' + JSON.stringify(data[0]['class']) + '   Axe : ' + JSON.stringify(data[0]['axe']))
+                        if (data.length) {
+                            message.channel.send({
+                                embed: {
+                                    color: 3447003,
+                                    author: {
+                                        name: bot.user.username,
+                                        icon_url: bot.user.avatarURL
+                                    },
+                                    title: "Gear of " + data[0]['family'],
+                                    fields: [{
+                                        name: 'Class',
+                                        value: data[0]['class']
+                                    },
+                                    {
+                                        name: 'AP',
+                                        value: JSON.stringify(data[0]['ap'])
+                                    },
+                                    {
+                                        name: 'DP',
+                                        value: JSON.stringify(data[0]['dp'])
+                                    },
+                                    {
+                                        name: 'Awakening AP',
+                                        value: JSON.stringify(data[0]['awakening'])
+                                    },
+                                    {
+                                        name: 'Trina Axe',
+                                        value: JSON.stringify(data[0]['axe'])
+                                    }
+                                    ]
+                                }
+                            })
+                        } else {
+                            message.channel.send('You have not entered any information. Please refer to !help for more info.')
+                        }
                     }
                 })
             }
@@ -221,6 +292,7 @@ bot.on('message', message => {
 
     }
 
+    return message.delete()
 });
 
 bot.login(botconfig.token)
