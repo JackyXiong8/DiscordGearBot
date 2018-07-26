b5b00cdc2d1aa6e1c9c6676d42d81e0bb6d1cc97
x <x@xs-MacBook-Pro-5.local>
2018-07-26 16:32:50 -0700

    Listening port 5000

--- index.js
+++ index.js
@@ -408,3 +408,8 @@ function insertionSort(array){
     }
     return flippedArray
 }
+
+var port = process.env.PORT || 5000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 5000");
+});
